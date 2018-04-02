34c0c1c8b305bed065a3c646fe466003ea99467f
Unknown <paarollver.12@gmail.com>
2018-04-02 15:39:08 -0400

    That's right

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  Hello
-// I like cheese!
+//
 //  Created by Shulin Shi on 4/1/18.
 //  Copyright © 2018 Shulin Shi. All rights reserved.
 //
@@ -10,34 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var buttonCount = 0
-    
-    
-    @IBOutlet weak var myLabel: UILabel!
-    
-    @IBAction func buttonTapped(_ sender: Any) {
-        buttonCount = buttonCount + 1
-        print(buttonCount)
-        
-        if buttonCount > 10 {
-            view.backgroundColor = UIColor.red
-            myLabel.text = "You hit more than 10 times!" 
-        }
-
-        
-    }
-    
-
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
